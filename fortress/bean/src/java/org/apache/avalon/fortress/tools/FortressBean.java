ef7344804177cc20dac9ce2cc1f96fa880c210b2
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2004-06-24 21:28:42 +0000

    add AvalonLogger because none of the commons logging jars contains it
    git-svn-id: https://svn.apache.org/repos/asf/excalibur/trunk@22089 13f79535-47bb-0310-9956-ffa450edef68

--- fortress/bean/src/java/org/apache/avalon/fortress/tools/FortressBean.java
+++ fortress/bean/src/java/org/apache/avalon/fortress/tools/FortressBean.java
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright 2003-2004 The Apache Software Foundation
  * Licensed  under the  Apache License,  Version 2.0  (the "License");
  * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at 
- * 
+ * You may obtain a copy of the License at
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed  under the  License is distributed on an "AS IS" BASIS,
  * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
  * implied.
- * 
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -42,7 +42,6 @@ import org.apache.avalon.framework.service.Serviceable;
 public class FortressBean implements Initializable, LogEnabled, Serviceable, Disposable {
 
     private static final String COMMONS_LOG_PROPERTY = "org.apache.commons.logging.Log";
-    private static final String COMMONS_AVALON_LOGGER = "org.apache.commons.logging.impl.AvalonLogger";
 
     private final FortressConfig config = new FortressConfig();
     private Logger logger = null;
@@ -89,7 +88,7 @@ public class FortressBean implements Initializable, LogEnabled, Serviceable, Dis
             // Get the root container initialized
             this.cm = new DefaultContainerManager(config.getContext());
             ContainerUtil.initialize(cm);
-            initializeCommonsLogging(cl);
+            initializeCommonsLogging();
             this.container = (DefaultContainer) cm.getContainer();
             this.sm = container.getServiceManager();
         }
@@ -99,22 +98,12 @@ public class FortressBean implements Initializable, LogEnabled, Serviceable, Dis
      * Use reflection to set up commons logging. If commons logging is available, it will be set up;
      * if it is not available, this section is ignored. This needs version 1.0.4 (or later) of commons
      * logging, earlier versions do not have avalon support.
-     * 
-     * @param cl class loader to look for commons logging classes
      */
-    private void initializeCommonsLogging(ClassLoader cl) {
+    private void initializeCommonsLogging() {
         try {
-            //if commons logging is available, set the static logger for commons logging
-            Class commonsLoggerClass;
-            if (cl != null) {
-                commonsLoggerClass = cl.loadClass(COMMONS_AVALON_LOGGER);
-            } else {
-                commonsLoggerClass = Class.forName(COMMONS_AVALON_LOGGER);
-            }
-            Method setDefaultLoggerMethod = commonsLoggerClass.getMethod("setDefaultLogger", new Class[] {Logger.class});
-            setDefaultLoggerMethod.invoke(null, new Object[] {cm.getLogger()});
-            //set the system property to use avalon logger
-            System.setProperty(COMMONS_LOG_PROPERTY, COMMONS_AVALON_LOGGER);
+            AvalonLogger.setDefaultLogger( cm.getLogger() );
+
+            System.setProperty(COMMONS_LOG_PROPERTY, AvalonLogger.class.getName());
         } catch (Exception e) {
             if (getLogger().isDebugEnabled()) getLogger().debug("error while initializing commons logging: " + e.getClass().getName() + ", " + e.getMessage());
         }
@@ -127,7 +116,7 @@ public class FortressBean implements Initializable, LogEnabled, Serviceable, Dis
     public static final String PROPERTY_CONTAINER_CLASS = "container.class";
     public static final String PROPERTY_CONTAINER_CONFIGURATION = "container.configuration";
     public static final String PROPERTY_CONTEXT_DIRECTORY = "context.directory";
-    public static final String PROPERTY_INSTRUMENT_MANAGER_CONFIGURATION = 
+    public static final String PROPERTY_INSTRUMENT_MANAGER_CONFIGURATION =
                                           "instrument.manager.configuration";
     public static final String PROPERTY_INVOKE_METHOD = "invoke.method";
     public static final String PROPERTY_LOGGER_MANAGER_CONFIGURATION = "logger.manager.configuration";
@@ -213,9 +202,9 @@ public class FortressBean implements Initializable, LogEnabled, Serviceable, Dis
     }
 
     /**
-     * The container implementation has to be a subclass of 
+     * The container implementation has to be a subclass of
      * <code>org.apache.avalon.fortress.impl.DefaultContainer</code>.
-     * 
+     *
      * @param containerClass fully qualified class name of the container implementation class.
      */
     public void setContainerClass(String containerClass) throws Exception {
