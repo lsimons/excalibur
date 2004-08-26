d95aeef82a9db97aeffa5a1136986938f6cf599c
hammant <hammant@13f79535-47bb-0310-9956-ffa450edef68>
2004-08-26 03:44:08 +0000

    Jon Elson-Green's patch for far dates
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk/planet/cornerstone/scheduler/impl@37053 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/org/apache/avalon/cornerstone/blocks/scheduler/TimeScheduledEntry.java
+++ src/main/org/apache/avalon/cornerstone/blocks/scheduler/TimeScheduledEntry.java
@@ -1,16 +1,16 @@
-/* 
+/*
  * Copyright 1999-2004 The Apache Software Foundation
  * Licensed  under the  Apache License,  Version 2.0  (the "License");
  * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at 
- * 
+ * You may obtain a copy of the License at
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed  under the  License is distributed on an "AS IS" BASIS,
  * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
  * implied.
- * 
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -117,7 +117,20 @@ public final class TimeScheduledEntry
     public int compareTo( final Object object )
     {
         final TimeScheduledEntry other = (TimeScheduledEntry)object;
-        return (int)-( other.m_time - m_time );
+		final long diff= other.m_time - m_time;
+		if( diff < 0 )
+		{
+			return 1;
+		}
+		else
+		if( diff == 0 )
+		{
+			return 0;
+		}
+		else
+		{
+			return -1;
+		}
     }
 
     public String toString()
