bc3f26bd0dd465da6a16bf8bb08feaa752d7c392
antonio <antonio@13f79535-47bb-0310-9956-ffa450edef68>
2005-07-31 01:07:28 +0000

    Fix typo
    git-svn-id: https://svn.apache.org/repos/asf/excalibur/trunk@226596 13f79535-47bb-0310-9956-ffa450edef68

--- components/datasource/src/java/org/apache/avalon/excalibur/datasource/ProxiedJdbcConnection.java
+++ components/datasource/src/java/org/apache/avalon/excalibur/datasource/ProxiedJdbcConnection.java
@@ -21,7 +21,7 @@ import java.lang.reflect.InvocationHandler;
 import java.sql.Connection;
 
 /**
- * A JDBC connection that has a proxy around it in order to dynamicly implement JDBC2/3 needs
+ * A JDBC connection that has a proxy around it in order to dynamically implement JDBC2/3 needs
  * to get a handle to its proxied self so that the proxied version can be returned to the pool.
  *
  * @author <a href="mailto:dev@avalon.apache.org">Avalon Development Team</a>
