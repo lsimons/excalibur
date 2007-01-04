200c411031be7b421b23430c4eedf6d77751d1d0
cziegeler <cziegeler@13f79535-47bb-0310-9956-ffa450edef68>
2007-01-04 08:25:51 +0000

    Cleanup and add new header
    git-svn-id: https://svn.apache.org/repos/asf/excalibur/trunk/components/sourceresolve@492442 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/excalibur/source/SourceFactory.java
+++ src/java/org/apache/excalibur/source/SourceFactory.java
@@ -1,16 +1,16 @@
-/* 
- * Copyright 2002-2004 The Apache Software Foundation
- * Licensed  under the  Apache License,  Version 2.0  (the "License");
- * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed  under the  License is distributed on an "AS IS" BASIS,
- * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
- * implied.
- * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -19,8 +19,6 @@ package org.apache.excalibur.source;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Map;
-
-import org.apache.avalon.framework.component.Component;
 
 /**
  * A source factory creates new source objects.
@@ -35,7 +33,6 @@ import org.apache.avalon.framework.component.Component;
  * @version $Id: SourceFactory.java,v 1.4 2004/02/28 11:47:26 cziegeler Exp $
  */
 public interface SourceFactory
-    extends Component
 {
     String ROLE = SourceFactory.class.getName();
 
--- src/java/org/apache/excalibur/source/SourceResolver.java
+++ src/java/org/apache/excalibur/source/SourceResolver.java
@@ -1,16 +1,16 @@
-/* 
- * Copyright 2002-2004 The Apache Software Foundation
- * Licensed  under the  Apache License,  Version 2.0  (the "License");
- * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed  under the  License is distributed on an "AS IS" BASIS,
- * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
- * implied.
- * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -19,8 +19,6 @@ package org.apache.excalibur.source;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Map;
-
-import org.apache.avalon.framework.component.Component;
 
 /**
  * Base interface for resolving a source by system identifiers.
@@ -43,7 +41,6 @@ import org.apache.avalon.framework.component.Component;
  */
 
 public interface SourceResolver
-    extends Component
 {
     String ROLE = SourceResolver.class.getName();
 
