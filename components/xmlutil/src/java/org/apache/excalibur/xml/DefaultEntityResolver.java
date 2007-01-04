99170518ad7e00955ee4040c605be22e68470c27
cziegeler <cziegeler@13f79535-47bb-0310-9956-ffa450edef68>
2007-01-04 08:31:15 +0000

    Cleanup and add new header
    git-svn-id: https://svn.apache.org/repos/asf/excalibur/trunk@492447 13f79535-47bb-0310-9956-ffa450edef68

--- components/xmlutil/src/java/org/apache/excalibur/xml/DefaultEntityResolver.java
+++ components/xmlutil/src/java/org/apache/excalibur/xml/DefaultEntityResolver.java
@@ -1,16 +1,16 @@
-/* 
- * Copyright 2002-2004 The Apache Software Foundation
- * Licensed  under the  Apache License,  Version 2.0  (the "License");
- * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed  under the  License is distributed on an "AS IS" BASIS,
- * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
- * implied.
- * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -19,7 +19,6 @@ package org.apache.excalibur.xml;
 import java.io.IOException;
 
 import org.apache.avalon.framework.activity.Disposable;
-import org.apache.avalon.framework.component.Component;
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.parameters.ParameterException;
 import org.apache.avalon.framework.parameters.Parameterizable;
@@ -57,8 +56,7 @@ public class DefaultEntityResolver extends AbstractLogEnabled
              Serviceable,
              Parameterizable,
              ThreadSafe,
-             Disposable,
-             Component {
+             Disposable {
 
     /** The catalog manager */
     protected CatalogManager catalogManager = new CatalogManager();
