ae052c042606a006e867cb5db0fc1e9a1e961fbe
cziegeler <cziegeler@13f79535-47bb-0310-9956-ffa450edef68>
2007-10-23 19:54:23 +0000

    Move test cases.
    git-svn-id: https://svn.apache.org/repos/asf/excalibur/trunk@587635 13f79535-47bb-0310-9956-ffa450edef68

--- components/sourceresolve/avalon/src/main/java/org/apache/excalibur/source/impl/CommonsVFSSourceFactory.java
+++ components/sourceresolve/avalon/src/main/java/org/apache/excalibur/source/impl/CommonsVFSSourceFactory.java
@@ -16,15 +16,9 @@
  */
 package org.apache.excalibur.source.impl;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.Map;
-
-import org.apache.avalon.framework.container.ContainerUtil;
 import org.apache.avalon.framework.logger.LogEnabled;
 import org.apache.avalon.framework.logger.Logger;
 import org.apache.avalon.framework.thread.ThreadSafe;
-import org.apache.excalibur.source.Source;
 
 /**
  * A factory for source types supported by
@@ -41,33 +35,6 @@ import org.apache.excalibur.source.Source;
 public class CommonsVFSSourceFactory extends org.apache.excalibur.source.factories.CommonsVFSSourceFactory
     implements ThreadSafe, LogEnabled
 {
-    /**
-     * Returns a {@link CommonsVFSSource} instance primed with the specified location
-     *
-     * @param location source location
-     * @param parameters source parameters
-     * @throws IOException if an IO error occurs
-     * @throws MalformedURLException if a URL is malformed
-     * @see org.apache.excalibur.source.SourceFactory#getSource(java.lang.String, java.util.Map)
-     */
-    public Source getSource( String location, Map parameters ) throws IOException, MalformedURLException
-    {
-        final Source source = new CommonsVFSSource( location, parameters );
-        ContainerUtil.enableLogging(source, m_logger);
-        return source;
-    }
-
-    /**
-     * Releases the given source.
-     *
-     * @param source source to release
-     * @see org.apache.excalibur.source.SourceFactory#release(org.apache.excalibur.source.Source)
-     */
-    public void release( Source source )
-    {
-        // Nothing to do here
-    }
-
     /**
      * Enables logging for this source.
      *
